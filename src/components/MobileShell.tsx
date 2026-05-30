1c85486e1caf5e70659eb19dfff0ec6cbf835303
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-05-30 05:59:45 +0200

    fix: horizontal overflow prevention on body and MobileShell

--- src/components/MobileShell.tsx
+++ src/components/MobileShell.tsx
@@ -1,6 +1,6 @@
 export default function MobileShell({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex-1 flex flex-col min-h-0">
+        <div className="flex-1 flex flex-col min-h-0 min-w-0">
             <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
                 {children}
             </div>
